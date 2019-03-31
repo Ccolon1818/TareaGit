c36e9224dff778de35593753c00f8268745ab024
ccolon1818 <ccolon1818@gmail.com>
2019-03-30 23:17:01 -0400

    'Commit'

---
+++ TareaKiut/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TareaKiut.Models;
+
+namespace TareaKiut.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET : /User/Index
+        public ActionResult Index()
+        {
+            return View(new User());
+        }
+
+
+        public ActionResult Profile(User user)
+        {
+            return View(user);
+        }
+
+        [HttpPost]
+        // GET : /User/Create
+        public ActionResult Index(User user)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(user);
+            }
+            return View("Profile", user);
+        }
+
+    }
+}
---
+++ TareaKiut/Models/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TareaKiut.Models
+{
+    public class User
+    {
+
+        [DisplayName("Cedula")]
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string IdNational { get; set; }
+
+        [DisplayName("Nombre")]
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string Name { get; set; }
+
+        [DisplayName("Apellido")]
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string LastName { get; set; }
+
+        [DisplayName("Correo Electronico")]
+        [EmailAddress]
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string Email { get; set; }
+
+        [DisplayName("Edad")]
+        [Range(15, 100)]
+        public int Age { get; set; }
+
+        [DisplayName("Estado Civil")]
+        public string Status { get; set; }
+
+        [DisplayName("Carrera")]
+        public string Career { get; set; }
+
+        [DisplayName("Numero De Telefono")]
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string PhoneNumber { get; set; }
+
+        [DisplayName("Hobbys")]
+        public string[] Hobbys { get; set; }
+
+        [DisplayName("Género:")]
+        public string Genero { get; set; }
+
+    }
+}
